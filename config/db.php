63643d7f2d778b2ca6bf31ea1df83d1570d10ca8
Víctor Cea Paredes <vicea@alumnos.ubiobio.cl>
2015-11-05 10:57:02 -0300

    Archivos base de yii framework
    se agregaron los archivos básicos de yii 2, con una configuracion local
    de sql server

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'sqlsrv:Server=VÍCTOR-PC;Database=Sistema',
+    'username' => '',
+    'password' => '',
+    'charset' => 'utf8',
+];
